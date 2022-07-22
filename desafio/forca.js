669c2f6be3fbe42c4cf56c1e3058608fe8b1157b
Arthur <arthur360thug@gmail.com>
2022-07-21 20:20:59 -0400

    adicionando funcionalidades,comentadas no codigo

--- desafio/forca.js
+++ desafio/forca.js
@@ -2,22 +2,38 @@ class Forca {
 
   //passando a palavra por um SPLIT para separar as letras 
   constructor(Forca){
-    this.caracters = Forca.split("");
+    this.characters = Forca.split("");
+    this.life = 0;
    }
 
   chutar(letra) { 
     //passando a palavra por um MAP para poder trocar a letra por um Underline "_"
-    this.boxes = this.caracters.map(char => {return "_"});
+    this.boxes = this.characters.map(char => {return "_"});
+    // criando função para
+    for(var i = 0; i < this.boxes.length; i++){
+      if(letra == this.characters[i]){
+        this.boxes[i] = letra;
+        console.log(this.boxes[i]);
+      } else {
+        console.log(this.boxes[i]);
+        life++;
+      }
+    }
+  
   }
 
   buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+   
+    console.log(this.boxes);
+    console.log(life)
+
+      //return {
+        //  letrasChutadas: [], // Deve conter todas as letras chutadas
+         // vidas: 6, // Quantidade de vidas restantes
+       //   palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
+      //}
   }
 }
 
