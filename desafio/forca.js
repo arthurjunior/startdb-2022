b3e8421b8660113ac6bafc07df436164e91db472
Arthur <arthur360thug@gmail.com>
2022-07-21 21:24:14 -0400

    corrigindo bugs

--- desafio/forca.js
+++ desafio/forca.js
@@ -3,31 +3,56 @@ class Forca {
   //passando a palavra por um SPLIT para separar as letras 
   constructor(Forca){
     this.characters = Forca.split("");
-    this.life = 0;
+    this.lives = 6;     // variavel vidas
+    this.corrects = 0; // variavel de acertos
+    
    }
 
   chutar(letra) { 
+    let gotRight = false;
+    let anyLeft = true;
     //passando a palavra por um MAP para poder trocar a letra por um Underline "_"
     this.boxes = this.characters.map(char => {return "_"});
     // criando função para
     for(var i = 0; i < this.boxes.length; i++){
       if(letra == this.characters[i]){
+        if(letra !== this.boxes[i]){
+          this.corrects++    // contador de acertos
+        }
         this.boxes[i] = letra;
         console.log(this.boxes[i]);
+        gotRight = true;
       } else {
         console.log(this.boxes[i]);
-        life++;
       }
     }
+
+    
   
   }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  buscarEstado() {
+    let gotRight = false;
+
+    if (gotRight == false) {this.lives--};
+    console.log("Acertou alguma" + gotRight);
+    console.log("Vidas restantes" + this.lives)
+
+    if (this.lives == 0){
+      console.log("Voce predeu");
+    } else {
+      if(this.corrects == this.characters.length){
+        console.log("Voce venceu");
+      } else {
+        console.log("Faltam letras ainda");
+      }
+    }
+   } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
    
     console.log(this.boxes);
-    console.log(life)
+    
 
       //return {
         //  letrasChutadas: [], // Deve conter todas as letras chutadas
