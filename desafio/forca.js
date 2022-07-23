d2bcc644b412e5b1f6747e1a0a0094ea235a884a
Arthur <arthur360thug@gmail.com>
2022-07-23 17:14:51 -0400

    corrigindo bugs e organizando codigo

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,20 +1,29 @@
 class Forca {
 
-  //passando a palavra por um SPLIT para separar as letras 
+  
   constructor(Forca){
-    this.characters = Forca.split("");
+   
+    this.characters = Forca.split(""); //Criei um array para passar a palavra por um SPLIT para separar as letras  em caracteres
     this.lives = 6;     // variavel vidas
     this.corrects = 0; // variavel de acertos
-    
+    this.chutes = [];  // 
    }
 
+   
+
   chutar(letra) { 
-    let gotRight = false;
-    let anyLeft = true;
-    //passando a palavra por um MAP para poder trocar a letra por um Underline "_"
-    this.boxes = this.characters.map(char => {return "_"});
+    this.chutes += letra;
+    
+    gotRight = false;
+    anyLeft = true;
+     //array para passae a palavra por um MAP para poder trocar a letra por um Underline "_"
+     this.boxes = this.characters.map(letra => {return "_"});
+
+    do{
+      
     // criando função para
-    for(var i = 0; i < this.boxes.length; i++){
+    for(let i = 0; i < this.boxes.length; i++){
+      
       if(letra == this.characters[i]){
         if(letra !== this.boxes[i]){
           this.corrects++    // contador de acertos
@@ -24,41 +33,45 @@ class Forca {
         gotRight = true;
       } else {
         console.log(this.boxes[i]);
+        
       }
     }
-
-    
+    if (gotRight == false) {this.lives--};
+  } while (this.live > 0);
   
   }
 
   buscarEstado() {
-    let gotRight = false;
+    
 
-    if (gotRight == false) {this.lives--};
-    console.log("Acertou alguma" + gotRight);
-    console.log("Vidas restantes" + this.lives)
+    
+    //console.log("Acertou alguma " + gotRight);
+    
 
     if (this.lives == 0){
-      console.log("Voce predeu");
+      return console.log("perdeu");
+      
     } else {
       if(this.corrects == this.characters.length){
-        console.log("Voce venceu");
+        return console.log("venceu");
       } else {
-        console.log("Faltam letras ainda");
+        console.log("Aguardando Chute");
       }
     }
    } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
 
   buscarDadosDoJogo() {
    
+    
+    console.log("Letras Chutadas " + this.chutes);
+    console.log("Vidas restantes " + this.lives); 
     console.log(this.boxes);
     
-
-      //return {
+      return {
         //  letrasChutadas: [], // Deve conter todas as letras chutadas
-         // vidas: 6, // Quantidade de vidas restantes
+          // vidas: 6, // Quantidade de vidas restantes
        //   palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      //}
+    }
   }
 }
 
